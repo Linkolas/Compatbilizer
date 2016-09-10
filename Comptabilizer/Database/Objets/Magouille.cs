13d30daf30d13353110bd2462cc288e73f3b876c
Nicolas <linkolas@live.fr>
2016-09-10 21:13:35 +0200

    Added missing protos

---
+++ Comptabilizer/Database/Objets/Magouille.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Comptabilizer.Database.Objets
+{
+    class Magouille
+    {
+        public int id = -1;
+        public int id_facture;
+        public int id_payeur;
+        public float valeur;
+        public string libelle;
+    }
+}
