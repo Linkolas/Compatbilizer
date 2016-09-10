83a4b016eea7fdf628cef240c561242cf7bdd196
Hansse Nicolas <nico.hansse@hotmail.fr>
2016-09-10 22:12:56 +0200

    ajout du user componant facture qui va dans dashboard

---
+++ Comptabilizer/view/component/FactureComponent.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Comptabilizer.view.component
+{
+    public partial class FactureComponent : UserControl
+    {
+        public FactureComponent()
+        {
+            InitializeComponent();
+        }
+
+        private void Avatar_Owner_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
