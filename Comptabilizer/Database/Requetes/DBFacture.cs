64e7b378089b0b09470a7669b706d27b59391617
Nicolas <linkolas@live.fr>
2016-09-10 21:30:11 +0200

    Generated dummy DB_ objects

---
+++ Comptabilizer/Database/Requetes/DBFacture.cs
@@ -0,0 +1,39 @@
+﻿using Comptabilizer.Database.Objets;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Comptabilizer.Database.Requetes
+{
+    class DBFacture : DB_Base<Facture>
+    {
+        public DBFacture(string table) : base(table) { }
+
+        public override int add(Facture obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool del(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Facture get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override List<Facture> getAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool set(int id, Facture obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Comptabilizer/Database/Requetes/DBMagouille.cs
@@ -0,0 +1,39 @@
+﻿using Comptabilizer.Database.Objets;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Comptabilizer.Database.Requetes
+{
+    class DBMagouille : DB_Base<Magouille>
+    {
+        public DBMagouille(string table) : base(table) { }
+
+        public override int add(Magouille obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool del(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Magouille get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override List<Magouille> getAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool set(int id, Magouille obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
