c20f0f6c3687ca53cbbeb80f37a61d5925557249
Nicolas <linkolas@live.fr>
2016-09-11 02:22:57 +0200

    Added commets for DBLog

--- Comptabilizer/Database/Requetes/DBLog.cs
+++ Comptabilizer/Database/Requetes/DBLog.cs
@@ -14,10 +14,23 @@ namespace Comptabilizer.Database.Requetes {
 		}
 
 		#region DB_Base implementation
+
+		/// <summary>
+		/// Ajoute un objet en BDD.
+		/// Si son ID est négatif, en génère un.
+		/// </summary>
+		/// <param name="f">Objet contenant les infos.</param>
+		/// <returns>L'ID de l'objet ajouté. En cas d'erreur, retourne -1.</returns>
 		public override int add(Log obj) {
 			throw new NotImplementedException();
 		}
 
+		/// <summary>
+		/// Modifie un objet en BDD.
+		/// </summary>
+		/// <param name="id">ID de l'objet à modifier.</param>
+		/// <param name="f">Objet contenant les nouvelles infos.</param>
+		/// <returns>True si la modif s'est bien passée, false sinon.</returns>
 		public override bool set(int id, Log obj) {
 			throw new NotImplementedException();
 		}
