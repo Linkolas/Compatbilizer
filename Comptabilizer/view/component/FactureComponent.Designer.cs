05922e4b099d4ba969170dec5ec2ade7557a51f1
Hansse Nicolas <nico.hansse@hotmail.fr>
2016-09-14 09:10:09 +0200

    ajout d'une bar de séparation au dessus du composant afin de différancier chaque facture quand elles sont listées

--- Comptabilizer/view/component/FactureComponent.Designer.cs
+++ Comptabilizer/view/component/FactureComponent.Designer.cs
@@ -35,16 +35,20 @@
             this.Avatar_Owner = new System.Windows.Forms.PictureBox();
             this.panel3 = new System.Windows.Forms.Panel();
             this.Value = new System.Windows.Forms.Label();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.panel5 = new System.Windows.Forms.Panel();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.Avatar_Owner)).BeginInit();
             this.panel3.SuspendLayout();
+            this.panel4.SuspendLayout();
+            this.panel5.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.Color.DimGray;
             this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
-            this.panel1.Location = new System.Drawing.Point(100, 0);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(1, 100);
             this.panel1.TabIndex = 1;
@@ -53,7 +57,7 @@
             // 
             this.panel2.BackColor = System.Drawing.SystemColors.ActiveBorder;
             this.panel2.Dock = System.Windows.Forms.DockStyle.Left;
-            this.panel2.Location = new System.Drawing.Point(101, 0);
+            this.panel2.Location = new System.Drawing.Point(1, 0);
             this.panel2.Name = "panel2";
             this.panel2.Size = new System.Drawing.Size(2, 100);
             this.panel2.TabIndex = 2;
@@ -62,11 +66,12 @@
             // 
             this.FacName.AutoSize = true;
             this.FacName.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.FacName.Location = new System.Drawing.Point(125, 32);
+            this.FacName.Location = new System.Drawing.Point(104, 25);
             this.FacName.Name = "FacName";
             this.FacName.Size = new System.Drawing.Size(131, 38);
             this.FacName.TabIndex = 3;
             this.FacName.Text = "FacName";
+            this.FacName.Click += new System.EventHandler(this.FacName_Click);
             // 
             // pictureBox1
             // 
@@ -75,7 +80,7 @@
             this.pictureBox1.Location = new System.Drawing.Point(727, 0);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Padding = new System.Windows.Forms.Padding(10);
-            this.pictureBox1.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox1.Size = new System.Drawing.Size(100, 97);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 4;
             this.pictureBox1.TabStop = false;
@@ -87,7 +92,7 @@
             this.Avatar_Owner.Location = new System.Drawing.Point(0, 0);
             this.Avatar_Owner.Name = "Avatar_Owner";
             this.Avatar_Owner.Padding = new System.Windows.Forms.Padding(10);
-            this.Avatar_Owner.Size = new System.Drawing.Size(100, 100);
+            this.Avatar_Owner.Size = new System.Drawing.Size(98, 97);
             this.Avatar_Owner.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.Avatar_Owner.TabIndex = 0;
             this.Avatar_Owner.TabStop = false;
@@ -99,38 +104,58 @@
             this.panel3.Dock = System.Windows.Forms.DockStyle.Right;
             this.panel3.Location = new System.Drawing.Point(560, 0);
             this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(167, 100);
+            this.panel3.Size = new System.Drawing.Size(167, 97);
             this.panel3.TabIndex = 5;
             // 
             // Value
             // 
             this.Value.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.Value.AutoSize = true;
             this.Value.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Value.Location = new System.Drawing.Point(19, 32);
+            this.Value.Location = new System.Drawing.Point(21, 26);
             this.Value.Name = "Value";
             this.Value.Size = new System.Drawing.Size(127, 38);
             this.Value.TabIndex = 0;
             this.Value.Text = "1000,00E";
             // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.Salmon;
+            this.panel4.Controls.Add(this.panel5);
+            this.panel4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel4.Location = new System.Drawing.Point(0, 0);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(827, 100);
+            this.panel4.TabIndex = 6;
+            // 
+            // panel5
+            // 
+            this.panel5.BackColor = System.Drawing.SystemColors.Control;
+            this.panel5.Controls.Add(this.panel3);
+            this.panel5.Controls.Add(this.Avatar_Owner);
+            this.panel5.Controls.Add(this.pictureBox1);
+            this.panel5.Controls.Add(this.FacName);
+            this.panel5.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel5.Location = new System.Drawing.Point(0, 3);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(827, 97);
+            this.panel5.TabIndex = 0;
+            // 
             // FactureComponent
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Controls.Add(this.panel3);
-            this.Controls.Add(this.pictureBox1);
-            this.Controls.Add(this.FacName);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
-            this.Controls.Add(this.Avatar_Owner);
+            this.Controls.Add(this.panel4);
             this.Name = "FactureComponent";
             this.Size = new System.Drawing.Size(827, 100);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.Avatar_Owner)).EndInit();
             this.panel3.ResumeLayout(false);
-            this.panel3.PerformLayout();
+            this.panel4.ResumeLayout(false);
+            this.panel5.ResumeLayout(false);
+            this.panel5.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -143,5 +168,7 @@
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Panel panel3;
         private System.Windows.Forms.Label Value;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Panel panel5;
     }
 }
--- Comptabilizer/view/component/FactureComponent.cs
+++ Comptabilizer/view/component/FactureComponent.cs
@@ -74,5 +74,10 @@ namespace Comptabilizer.view.component
         {
 
         }
+
+        private void FacName_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
